f9b6a2d83591f5a421b9b7b2844425d1f5e6c185
G.G.S <82466341+Zer0plusOne@users.noreply.github.com>
2024-01-15 14:26:57 +0100

    Update GESTOR_DE_COMENATIOS.sh

--- GESTOR_DE_COMENATIOS.sh
+++ GESTOR_DE_COMENATIOS.sh
@@ -1,50 +1,27 @@
 #!/bin/bash
 
-# Usuarios de la base de datos
-DB_USER=""
-DB_PASS=""
-
-echo "LOGIN COMMENT_MANAGER"
-echo "user:"
-read $DB_USER
-read "password:"
-read $DB_PASS
-
-if [ "$DB_USER" != comentarios ]
-then
-    echo "Usuario no valido para el gestor, saliendo del programa..."
-    wait 1
-    clear
-    exit 1
-fi
-clear
-
+DB_USER="comentar"
 DB_NAME="faryadventures"
 
-echo "Bienvenido al gestor de commentarios de la base de datos Faryadventures"
-echo "Seleccione que accion deseas realizar:"
+echo "Bienvenido al gestor de comentarios de la base de datos Faryadventures"
+echo "Seleccione qué acción desea realizar:"
 echo "1. Leer comentarios existentes"
 echo "2. Insertar comentarios"
 
-read $SELECCION
+read SELECCION
 
-if [ "$SELECCION" == "1" ]
-then
-
+if [ "$SELECCION" == "1" ]; then
     SQL_QUERY="SELECT * FROM comments;"
-
-    mysql -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "$SQL_QUERY"
+    mysql -u "$DB_USER" -D "$DB_NAME" -e "$SQL_QUERY"
 fi
 
-if [ "$SELECCION" == "2" ]
-then 
+if [ "$SELECCION" == "2" ]; then 
     echo "Escriba el comentario a insertar:"
-    read $COMENTARIO
+    read COMENTARIO
 
-    SQL_QUERY="INSERT INTO comments (comment) VALUES ("$COMENTARIO") "
-
-    mysql -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -e "$SQL_QUERY"
-    wait 1
-    echo "Commentario añadido exitosamente"
+    SQL_QUERY="INSERT INTO comments (comment) VALUES ('$COMENTARIO');"
+    mysql -u "$DB_USER" -D "$DB_NAME" -e "$SQL_QUERY"
+    echo "Comentario añadido exitosamente"
 fi
+
 exit 0
